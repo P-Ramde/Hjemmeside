70b5d93aa1da47a8c84c1dd73422f31cdbae3a12
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-02-28 21:17:27 +0100

    Oppdatering av fil lokasjon

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-name = prompt ("Vennligst fyll inn navnet ditt:")
-var welcome = "Hei" +name+ " og velkommen til min side. Håper du finner noe du liker."
+name = prompt ("Vennligst fyll inn navnet ditt:");
+var welcome = "Hei" +name+ " og velkommen til min side. Håper du finner noe du liker.";
 alert (welcome);
