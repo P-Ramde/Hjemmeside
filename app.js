59a9797a2cbd616627a76716962ada158602804b
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-02-28 21:21:41 +0100

    Korrigering 2

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-name = prompt ("Vennligst fyll inn navnet ditt:");
+var name = prompt ("Vennligst fyll inn navnet ditt:");
 var welcome = "Hei" + name + " og velkommen til min side. Håper du finner noe du liker.";
 alert (welcome);
