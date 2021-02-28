069da00ef7756ea01d2785e06a5321c0c5ba1a5e
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-02-28 21:37:39 +0100

    test

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var name = prompt ("Vennligst fyll inn navnet ditt:");
 var welcome = "Hei " + name + " og velkommen til min side. Håper du finner noe du liker.";
-alert (welcome);
+document.write (welcome);
