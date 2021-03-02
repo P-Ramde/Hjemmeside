763511a159403bc422322b073f5fc845d7fd34e9
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-03-02 23:03:36 +0100

    Button update

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var name = prompt ("Vennligst fyll inn navnet ditt:");
-var welcome = "<p>Hei " + name + " og velkommen til min side. Håper du finner noe du liker.</p>";
-document.write(welcome);
+//var name = prompt ("Vennligst fyll inn navnet ditt:");
+//var welcome = "<p>Hei " + name + " og velkommen til min side. Håper du finner noe du liker.</p>";
+//document.write(welcome);
+
+var cv = document.getElementById('cv');
+var work = document.getElementById('work');
+
+cv.addEventListener("click", function () {
+  work.textContent = "11.05.2020 - D.D, Zimmer & Peacock AS, Ingeniør";
+});
