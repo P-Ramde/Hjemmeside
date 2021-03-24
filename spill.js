5d50854ac46120879934547b2b7bac9b16d7def0
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-03-24 22:02:24 +0100

    Upgrade on 3 på rad code

--- spill.js
+++ spill.js
@@ -1,109 +1,77 @@
-var boxes = document.getElementById("boxes")
+var boxes = document.getElementById("boxes");
 var listNumbers = [0,1,2,3,4,5,6,7,8];
-//var squares = [0,1,2,3,4,5,6,7,8]; // Names each square
-var human = "box boxHuman"
-var computer = "box boxComputer"
+var human = "box boxHuman";
+var computer = "box boxComputer";
 
-// Setter id 0 til 8 inn i boks 1 til 9.
-for (var x=0; x< boxes.children.length; x++)
-{
-  boxes.children[x].setAttribute("id",x)
+for (var x=0; x< boxes.children.length; x++){
+  boxes.children[x].setAttribute("id",x);
 }
 
-for (var i = 0; i < boxes.children.length; i++)
-{
+for (var i = 0; i < boxes.children.length; i++){
+  boxes.children[i].addEventListener("click", function(){
+    this.className += " boxHuman";
 
-  boxes.children[i].addEventListener("click", function()
-  {
-    //alert(i)
-    this.className += " boxHuman"
-    var tall = listNumbers.indexOf(parseInt(this.id));
-    //alert("id " + this.id)
-    //alert("nr i listen " + tall)
-    if(tall > -1)
-    {
-      listNumbers.splice(tall,1);
+    var number = listNumbers.indexOf(parseInt(this.id));
+
+    if(number > -1){
+      listNumbers.splice(number,1);
     }
-    //alert(boxes.children[0].className)
-    if(winningComboHuman() == true){
-      setTimeout(function(){
-        alert("Gratulerer du vant")
-        return restart()
-      },200)
+
+    if(winningCombo(human)){
+        endGame("Gratulerer du vant");
     }
-    if (listNumbers==0 && winningComboHuman() == false){
-      setTimeout(function(){
-        alert("Uavgjort")
-        return restart();
-      },200)
+
+    if (listNumbers.length == 0 && !winningCombo(human)){
+        endGame("Uavgjort");
     }
-    //alert(listNumbers.length)
-    if(listNumbers.length>0 && winningComboHuman() == false)
-    {
-      var valgt = Math.floor(Math.random()*listNumbers.length)
-      //alert("nr i listen " + valgt)
-      var test = listNumbers[valgt];
-      //alert("id " + test)
-      document.getElementById(test).className += " boxComputer"
-      if(valgt > -1)
-      {
-        listNumbers.splice(valgt,1);
+
+    if(listNumbers.length > 0 && !winningCombo(human)){
+      var random = Math.floor(Math.random()*listNumbers.length)
+      var chosen = listNumbers[random];
+
+      document.getElementById(chosen).className += " boxComputer"
+
+      if(random > -1){
+        listNumbers.splice(random,1);
       }
     }
-    if(winningComboComputer()==true){
-      setTimeout(function(){
-        alert("Du tapte desverre")
-        return restart()
-      },200)
-    }
+
+    if(winningCombo(computer)){
+        endGame("Du tapte desverre")
+      }
   })
-  // Gjør at det blir en sirkel når musen beveger seg over.
-  boxes.children[i].addEventListener("mouseover", function()
-  {
+
+  boxes.children[i].addEventListener("mouseover", function(){
     this.style.backgroundImage = "url('img/o.svg')";
   })
-  // Gjør at sirkelen forvinner når musen fjernes
-  boxes.children[i].addEventListener("mouseout", function()
-  {
+
+  boxes.children[i].addEventListener("mouseout", function(){
     this.style.backgroundImage = "";
   })
 }
-function winningComboHuman(){
+
+function winningCombo(player){
   if (
-    (boxes.children[0].className == human && boxes.children[1].className == human && boxes.children[2].className == human) ||
-    (boxes.children[3].className == human && boxes.children[4].className == human && boxes.children[5].className == human) ||
-    (boxes.children[6].className == human && boxes.children[7].className == human && boxes.children[8].className == human) ||
-    (boxes.children[0].className == human && boxes.children[3].className == human && boxes.children[6].className == human) ||
-    (boxes.children[1].className == human && boxes.children[4].className == human && boxes.children[7].className == human) ||
-    (boxes.children[2].className == human && boxes.children[5].className == human && boxes.children[8].className == human) ||
-    (boxes.children[0].className == human && boxes.children[4].className == human && boxes.children[8].className == human) ||
-    (boxes.children[2].className == human && boxes.children[4].className == human && boxes.children[6].className == human)
+    (boxes.children[0].className == player && boxes.children[1].className == player && boxes.children[2].className == player) ||
+    (boxes.children[3].className == player && boxes.children[4].className == player && boxes.children[5].className == player) ||
+    (boxes.children[6].className == player && boxes.children[7].className == player && boxes.children[8].className == player) ||
+    (boxes.children[0].className == player && boxes.children[3].className == player && boxes.children[6].className == player) ||
+    (boxes.children[1].className == player && boxes.children[4].className == player && boxes.children[7].className == player) ||
+    (boxes.children[2].className == player && boxes.children[5].className == player && boxes.children[8].className == player) ||
+    (boxes.children[0].className == player && boxes.children[4].className == player && boxes.children[8].className == player) ||
+    (boxes.children[2].className == player && boxes.children[4].className == player && boxes.children[6].className == player)
   ){
     return true;
-  } else {
-    return false;
   }
 }
-function winningComboComputer(){
-  if (
-    (boxes.children[0].className == computer && boxes.children[1].className == computer && boxes.children[2].className == computer) ||
-    (boxes.children[3].className == computer && boxes.children[4].className == computer && boxes.children[5].className == computer) ||
-    (boxes.children[6].className == computer && boxes.children[7].className == computer && boxes.children[8].className == computer) ||
-    (boxes.children[0].className == computer && boxes.children[3].className == computer && boxes.children[6].className == computer) ||
-    (boxes.children[1].className == computer && boxes.children[4].className == computer && boxes.children[7].className == computer) ||
-    (boxes.children[2].className == computer && boxes.children[5].className == computer && boxes.children[8].className == computer) ||
-    (boxes.children[0].className == computer && boxes.children[4].className == computer && boxes.children[8].className == computer) ||
-    (boxes.children[2].className == computer && boxes.children[4].className == computer && boxes.children[6].className == computer)
-  ){
-    return true;
-  } else {
-    return false;
-  }
-}
-function restart(){
-  listNumbers = [0,1,2,3,4,5,6,7,8]
-  for (var y = 0; y < boxes.children.length; y++){
-    boxes.children[y].className = boxes.children[y].className.replace(" boxHuman", "");
-    boxes.children[y].className = boxes.children[y].className.replace(" boxComputer", "");
-  }
+
+function endGame(message){
+  setTimeout(function(){
+    alert(message);
+    listNumbers = [0,1,2,3,4,5,6,7,8];
+    for (var y = 0; y < boxes.children.length; y++){
+      boxes.children[y].className = boxes.children[y].className.replace(" boxHuman", "");
+      boxes.children[y].className = boxes.children[y].className.replace(" boxComputer", "");
+    }
+  },500)
 }
