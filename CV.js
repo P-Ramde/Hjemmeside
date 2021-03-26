2c51e51aed5a1a756f93d1bca971e16bf34fc72f
P-Ramde <63366121+P-Ramde@users.noreply.github.com>
2021-03-26 21:50:21 +0100

    Color update and sidebar

---
+++ CV.js
@@ -0,0 +1,6 @@
+var cv = document.getElementById('cv');
+var work = document.getElementById('work');
+
+cv.addEventListener("click", function () {
+  work.textContent = "11.05.2020 - D.D, Zimmer & Peacock AS, Ingeniør";
+});
